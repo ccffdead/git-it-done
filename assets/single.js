62ce3b4c31dd9f28a8c3520474a4ee0531e19516
ccffdead <ccffdead@gmail.com>
2021-11-13 22:10:45 -0500

    added pagination issues

--- assets/single.js
+++ assets/single.js
@@ -1,4 +1,7 @@
+var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
+
+
 
 var getRepoIssues = function (repo) {
     console.log(repo);
@@ -9,6 +12,10 @@ var getRepoIssues = function (repo) {
             response.json().then(function (data) {
                 // pass response data to dom function
                 displayIssues(data);
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -21,7 +28,7 @@ var displayIssues = function (issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
-      }
+    }
     for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
@@ -51,6 +58,16 @@ var displayIssues = function (issues) {
         issueEl.setAttribute("target", "_blank");
         issueContainerEl.appendChild(issueEl);
     }
+};
+
+var displayWarning = function (repo) {
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
